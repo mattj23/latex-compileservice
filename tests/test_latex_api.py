86c9155f63644d6545ea7301da149d8a7601b600
Matt Jarvis <mattj23@gmail.com>
2019-10-13 16:55:53 -0400

    first test runs through runner

--- tests/test_latex_api.py
+++ tests/test_latex_api.py
@@ -1,1 +1,18 @@
+import unittest
+from latex import create_app
+from latex.config import TestConfig
+
+
+class LatexApiTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.app = create_app(TestConfig())
+        self.client = self.app.test_client()
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_session_endpoint_routes_correctly(self):
+        response = self.client.get("/api/sessions", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
